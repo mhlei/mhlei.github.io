cf74f1a8ed9b9eeb1ab826dbba164dbebc31eae6
雷明环 <leiminghuan@126.com>
2019-07-21 15:33:38 +0800

    -

--- devops/mk.sh
+++ devops/mk.sh
@@ -3,11 +3,11 @@
 cmd_dir=`dirname ${0}`
 curr_dir=`cd $cmd_dir && pwd`
 dirname=${curr_dir##*/}
-echo "##$dirname"
+echo "##$dirname\n"
 for f in `ls`
 do
   if [ "$f" != "mk.sh" ] && [ "$f" != "index.md" ]; then
-    echo "[$f](./$f)";
+    echo "[$f](./$f)|";
   fi
 done
 
