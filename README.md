ac6f49b3e123ce536f017d8d67c5e0a178b2f552
雷明环 <leiminghuan@126.com>
2019-07-21 15:30:47 +0800

    mkdir auto generate index

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## articles
-[mtux-config.md](article/mtux-config.md)
-[devops抽象分支](article/抽象分支.md)
+[About DevOps](./devops)
 
 ## links for study
 [edit this page](https://github.com/mhlei/mhlei.github.io/edit/master/README.md)
---
+++ devops/mk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cmd_dir=`dirname ${0}`
+curr_dir=`cd $cmd_dir && pwd`
+dirname=${curr_dir##*/}
+echo "##$dirname"
+for f in `ls`
+do
+  if [ "$f" != "mk.sh" ] && [ "$f" != "index.md" ]; then
+    echo "[$f](./$f)";
+  fi
+done
+
