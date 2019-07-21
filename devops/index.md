6e07e57f2f1f437b9f297bce1506adbc4c6d61a3
雷明环 <leiminghuan@126.com>
2019-07-21 15:37:20 +0800

    -

--- devops/index.md
+++ devops/index.md
@@ -1,3 +1,3 @@
-##devops\n
+## devops  
 [mtux-config.md](./mtux-config.md)|
 [抽象分支.md](./抽象分支.md)|
--- devops/mk.sh
+++ devops/mk.sh
@@ -3,7 +3,7 @@
 cmd_dir=`dirname ${0}`
 curr_dir=`cd $cmd_dir && pwd`
 dirname=${curr_dir##*/}
-echo "##$dirname\n"
+echo "## $dirname  "
 for f in `ls`
 do
   if [ "$f" != "mk.sh" ] && [ "$f" != "index.md" ]; then
